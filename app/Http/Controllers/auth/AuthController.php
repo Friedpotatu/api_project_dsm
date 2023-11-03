3adc0b698f00787e3e44c76030958cc6876d1761
Friedpotatu <nicolas.pavez@alumnos.ucn.cl>
2023-11-03 01:10:12 -0300

    first commit

---
+++ app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+
+        $credentials = $request->only('email', 'password');
+
+        try {
+            //Intentar autenticar al usuario con las credenciales recibidas.
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'message' => 'las credenciales ingresadas son incorrectas.'
+                ]);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'token no creado'
+            ], 500);
+        }
+        return response()->json(compact('token'));
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\user\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('jwt.verify')->group(function () {
+    Route::get('users', [UserController::class, 'index']);
+    Route::post('post', [PostController::class, 'store']);
+});
